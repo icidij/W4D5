f8eefdf06b2c22720ab5e4cd1533f1f80af5c7c7
icidij <jamesc1288@gmail.com>
2021-09-03 14:30:17 -0400

    BigO Project finished - about to begin anagrams

--- Classwork.rb
+++ Classwork.rb
@@ -9,7 +9,11 @@ def my_min_v1(arr)
 
     # debugger
     arr.each do |num1|
-        min < num1 ? min : min = num1
+        dup_arr = arr.dup
+        dup_arr.delete(num1)
+        min = num1 if dup_arr.all?{|ele| ele > num1}
+        
+        # min < num1 ? min : min = num1
     end
 
     min
@@ -24,6 +28,41 @@ def my_min_v2(arr)
 
 end
 
+def lgsv1(arr) 
 
-# p my_min_v1([ 0, 3, 5, 4, -5, 10, 1, 90 ])
-p my_min_v2([ 0, 3, 5, 4, -5, 10, 1, 90 ])
+    subarrays = []
+    arr.each_index do |i1|
+        (i1...arr.length).each do |i2|
+            subarrays << arr[i1..i2]
+        end
+    end
+
+    sums = subarrays.map do |sub|
+                sub.inject do |acc, ele|
+            acc + ele
+        end
+    end
+    sums.max
+end
+
+
+def lgsv2(arr)
+    largest_sum = arr.first
+
+    current_sum = arr.first
+    (1...arr.length).each do |i|
+        if arr[i] < 0
+            current_sum = 0
+        end
+        current_sum += arr[i]
+        if current_sum > largest_sum
+            largest_sum = current_sum           
+        end
+    end
+    largest_sum
+end
+
+
+
+
+
